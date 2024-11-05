2a1717e34bc225192c02957050c973c40ae75eb8
Chanirur <chaniru.ranasinghe123@gmail.com>
2024-11-05 22:02:18 +0000

    add libs and sturct for postgre + add a basic home page

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
+# Ensure you set the DATABASE_URI correctly
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+# Initialize SQLAlchemy
+db = SQLAlchemy(app)
+
+class User(db.Model, UserMixin):  # Renamed from 'users' to 'User' for convention
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
 @app.route('/')
 def index():
-    return "this works"
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
@@ -14,6 +33,6 @@ def about():
 def login():
     return "login works"
 
-@app.route('/   register')
+@app.route('/register')
 def register():
     return "register works"
