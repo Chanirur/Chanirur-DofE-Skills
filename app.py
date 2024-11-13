0ccd6ba637166cd1a5aeeacd3ab6654792a4dde3
Chanirur <chaniru.ranasinghe123@gmail.com>
2024-11-13 18:24:12 +0000

    switch to mySQL and add a cert

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from dotenv import load_dotenv
+import ssl
 import os
 
 # Load environment variables from .env file
@@ -9,18 +10,27 @@ load_dotenv()
 
 app = Flask(__name__)
 
+# Configure SSL context with only the ca.pem file
+ssl_context = ssl.create_default_context(cafile='ca.pem')
+
 # Ensure you set the DATABASE_URI correctly
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-print("URI")
 print(app.config['SQLALCHEMY_DATABASE_URI'])
-print("SEC")
 print(app.config['SECRET_KEY'])
+
+#Ensures the use of only the CA for ssl
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'connect_args': {
+        'ssl': ssl_context
+    }
+}
+
 # Initialize SQLAlchemy
 db = SQLAlchemy(app)
 
-class User(db.Model, UserMixin):  # Renamed from 'users' to 'User' for convention
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(80), nullable=False)
@@ -40,3 +50,8 @@ def login():
 @app.route('/register')
 def register():
     return "register works"
+
+@app.route('/createdb')
+def create_db():
+    db.create_all()
+    return "db table created"
